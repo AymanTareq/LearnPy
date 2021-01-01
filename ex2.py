fcd21fb56bb1532195e95a4cd7cc55ea8e1bb257
AymanTareq <tareq.ctec02@gmail.com>
2021-01-01 14:35:30 +0600

    Exercise 2 done

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# This is a comment.
+# Comment in a script is very essential element.
+# comments are ingored by python interpreter
+
+print('Make comment to make your code more readable and understandable!')
+print('Another reason for using comment is to debug code.')
