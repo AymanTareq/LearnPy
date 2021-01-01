6f68f7a23bbd914aa364c0b1e43c8396ddd598f3
AymanTareq <tareq.ctec02@gmail.com>
2021-01-01 15:41:29 +0600

    Excercise 3 done

---
+++ ex3.py
@@ -0,0 +1,13 @@
+print("I will now count my chickens!")
+print("Hens", 25 + 30/6)                # Result: 30
+print("Roosters",100 -25 * 3 % 4)       # Result: 97
+print("Now I will count the eggs: ")
+
+print(3+2+1-5+4%2-1/4+6)                # Result: 6.75
+
+print("Is it true that 3 + 2 < 5 - 7 ?")
+print(3+2 < 5-7)
+print("What is 3 + 2?",3+2)
+print("What is 5-7?",5-7)
+
+print("Oh, I see! What going on!")
