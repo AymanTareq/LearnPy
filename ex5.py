0c768c07ec52546aee2a5be432bc2a4932bad868
AymanTareq <tareq.ctec02@gmail.com>
2021-01-03 00:40:34 +0600

    Exercise 4,5,6 done

---
+++ ex5.py
@@ -0,0 +1,11 @@
+my_name = 'Ayman Tareq'
+my_age = 23
+my_height = 64      # inchs
+my_weight = 59      # kg
+
+print(f"Hey, this is {my_name}.I'm {my_age} years old.My height is {my_height} inches and my weight is {my_weight} kg.")
+my_height_cm = round(my_height * 2.54)         # cm unit
+my_weight_lbs = round(my_weight * 2.20462)     # converted to lbs
+
+print(f"My weight in pounds is {my_weight_lbs} lbs and height in cm unit is {my_height_cm} .")
+        
