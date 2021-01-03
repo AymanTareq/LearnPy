48b36e8ef20f2c15d8987c9af1e6529e163b620a
AymanTareq <tareq.ctec02@gmail.com>
2021-01-03 19:35:01 +0600

    exercise 9 done

---
+++ ex9.py
@@ -0,0 +1,11 @@
+# exercise 9
+days = "Sat Sun Mon Tue Wed Thu Fri"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun"
+
+print("Here are the days: ",days)
+print("Here are the months: ",months)
+
+print("""
+Where there is a will,
+there is a way.
+""")
