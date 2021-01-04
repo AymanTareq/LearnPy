021356e13ad3093a4c2d456bcd54bbd27b7e12cd
AymanTareq <tareq.ctec02@gmail.com>
2021-01-05 01:07:08 +0600

    More exercise on exercise 10

--- ex10.py
+++ ex10.py
@@ -15,3 +15,19 @@ print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+# Exercise from my own
+
+txt = """
+\tThis is a backslash (\\).
+\tThis is single-quote (\').
+\tThis is Double-quote(\").
+\tThis is ASCII bell (\a).
+\tThis is ASCII backspace (\b)
+\tThis is vertical tab\v
+\tThis is 16-bit hex value for \u2665 shape! cool \u10E6
+\tThis is octal value for (Hello) \110\145\154\154\157
+
+"""
+
+print(txt)
