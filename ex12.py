fcf62a8c401b011275d108cea425c9194f9cd6fc
AymanTareq <tareq.ctec02@gmail.com>
2021-01-05 10:48:05 +0600

    exercise 11 and 12 done

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# exercise 12
+
+age = input("\tHow old are you? ")
+height = input("\tHow tall are you? ")
+weight = input("\tHow much do you weigh (kg)? ")
+
+print("\tSo, you're {} years old, {} tall and {} kg heavy.".format(age,height,weight))
