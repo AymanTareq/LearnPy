cf63e304a146ae74f531824b602702de98c64c23
AymanTareq <tareq.ctec02@gmail.com>
2021-01-24 23:10:03 +0600

    done

---
+++ ex34.py
@@ -0,0 +1,12 @@
+## Exercise 34. Accessing Elements of Lists
+
+animals = ['bear','tiger','penguin','zebra','kangaroo','whale','platypus']
+bear = animals[0]
+
+print('Animal list:',animals)
+print('The animal at 1:',animals[1])
+print("The third animal:",animals[2])
+print("The first animal:",animals[0])
+print("The animal at 3:",animals[3])
+print("The fifth animal:",animals[4])
+print("The animal at 4:",animals[4])
