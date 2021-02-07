e9198c03ce62dec9199fe88cdc9786327643083e
AymanTareq <tareq.ctec02@gmail.com>
2021-02-07 21:51:58 +0600

    Exercise 41 done

--- test_code.py
+++ test_code.py
@@ -1,5 +1,35 @@
 
 
+# a = 'tareq'
+# b = 55
+# class Song(object):
+
+#     def __init__(self, lyrics,num):
+#         self.lyrics = lyrics
+#         self.num = num
+
+#     def sing_me_a_song(self):
+#         print("Value of b: ",b*10)        
+#         print("Value of a: ",a)
+#         print("Value of num and local variable: ", self.num + b)      
+
+#         for line in self.lyrics:
+#             print(line)
+
+# song = ["Happy birthday to you",
+#         "I don't want to get sued",
+#         "So I'll stop right there"    
+#         ]
+
+# happy_bday = Song(song,34)
+# bulls_on_parade = Song(["There rally around tha family","With pockets full of shells"],100)
+
+# happy_bday.sing_me_a_song()
+
+# bulls_on_parade.sing_me_a_song()
+
+
+
 
 # # This script demonstrate the `flush` argument of print() function.
 # #!/usr/bin/env python
@@ -20,3 +50,259 @@
 # for i in "Ayman Tareq":
 #     print(i,end='',flush=True)
 #     sleep(.1)
+
+
+
+a = """
+account
+achiever
+actor
+addition
+adjustment
+advertisement
+advice
+aftermath
+agreement
+airplane
+airport
+alarm
+amount
+amusement
+angle
+animal
+answer
+ant
+apparatus
+apparel
+apple
+appliance
+approval
+arch
+argument
+arithmetic
+arm
+army
+art
+attack
+attempt
+attention
+attraction
+aunt
+authority
+baby
+back
+badge
+bag
+bait
+balance
+ball
+balloon
+banana
+band
+base
+baseball
+basket
+basketball
+bat
+bath
+battle
+bead
+beam
+bean
+bear
+beast
+bed
+bedroom
+bee
+beef
+beetle
+beggar
+beginner
+behavior
+belief
+believe
+bell
+berry
+bike
+bird
+birth
+birthday
+bit
+bite
+blade
+blood
+blow
+board
+boat
+body
+bomb
+bone
+book
+boot
+border
+bottle
+boundary
+box
+boy
+brain
+brake
+branch
+brass
+bread
+breakfast
+breath
+brick
+bridge
+brother
+brush
+bubble
+bucket
+building
+bulb
+bun
+burn
+burst
+business
+butto
+cabbage
+cable
+cactus
+cake
+cakes
+calculator
+calendar
+camera
+camp
+can
+cannon
+canvas
+cap
+caption
+car
+card
+carpenter
+carriage
+cart
+cast
+cat
+cattle
+cause
+cave
+celery
+cellar
+cemetery
+cent
+chain
+chair
+chalk
+chance
+change
+channel
+cheese
+cherry
+chess
+chicken
+children
+chin
+church
+circle
+clam
+clock
+cloth
+cloud
+clover
+club
+coach
+coal
+coast
+coat
+cobweb
+coil
+collar
+color
+comb
+comfort
+committee
+company
+competition
+condition
+connection
+control
+cook
+copper
+copy
+cord
+cork
+corn
+cough
+country
+cover
+cow
+crack
+cracker
+crate
+crayon
+cream
+creator
+creature
+credit
+crib
+crime
+crook
+crow
+crowd
+crown
+crush
+cry
+cub
+cup
+current
+curtain
+curve
+cushion
+dad
+daughter
+day
+death
+debt
+decision
+deer
+degree
+design
+desire
+desk
+destruction
+detail
+development
+digestion
+dime
+dinner
+dinosaurs
+direction
+dirt
+discovery
+discussion
+disease
+disgust
+distance
+distribution
+division
+dock
+doctor
+dog
+dogs
+doll
+donkey
+door
+downtown
+drain
+drawer
+dress
+drink
+driving
+drop
+drug
+drum
+duck
+dust
+"""
